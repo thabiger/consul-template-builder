cbb05b2962dd291a90cf15b64c839be35fa0b4b3
Tomasz Habiger <thabiger@trans.eu>
2021-02-08 06:44:36 +0100

    first commit

---
+++ env.sh
@@ -0,0 +1,6 @@
+export GIT_AUTHOR_NAME="Tomasz Habiger"
+export GIT_AUTHOR_EMAIL="tomasz.habiger@gmail.com"
+
+export UPSTREAM_VERSION="0.25.1"
+export CONSUL_VERSION="1.8.0"
+export VAULT_VERSION="1.4.2"
